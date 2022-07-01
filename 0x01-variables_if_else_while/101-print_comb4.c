9d4906caa5ea9036fa99ec0529d85b51d7ad1691
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-01 15:56:05 -0700

    print three numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+  * main - Prints three numbers
+  *
+  * Return: return 0 (Success)
+  */
+int main(void)
+{
+	int j;
+	int i;
+	int k;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (j < i && i < k)
+				{
+					putchar(j);
+					putchar(i);
+					putchar(k);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
