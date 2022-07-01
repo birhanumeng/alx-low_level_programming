074b8eaf7d17619b969431d29000d03435c509ba
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-01 14:23:16 -0700

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 0-9
+ *
+ * Return: rturn 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf(i);
+
+	printf('\n');
+
+	return (0);
+}
