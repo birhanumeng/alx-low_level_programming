4a3959b6b825ad38cbcafead1167e119a67981dc
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-01 17:33:26 -0700

    print combination of pair numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combination of pair numbers
+ *
+ * Return: rturn 0 (Success)
+ */
+int main(void)
+{
+	int j;
+	int i;
+	int k;
+	int m;
+
+	for (j = 0; j <= 9; j++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			for (k = 0; k <= 9; k++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+
+				        if ((j == i) && (i == k) && (k == m))
+						continue;
+
+					putchar(j);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(m);
+
+					if ((j == 9) && (i == 9) && (k == 9) && (m == 8))
+						break;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
