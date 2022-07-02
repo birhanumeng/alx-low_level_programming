874cd3bfcfcbb5f401fd47c9713ba4743ef796d3
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-01 18:37:24 -0700

    print combination of pair numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar(num1 / 10);
-			putchar(num1 % 10);
+			putchar((num1 / 10));
+			putchar((num1 % 10));
 			putchar(' ');
-			putchar(num2 / 10);
-			putchar(num2 % 10);
+			putchar((num2 / 10));
+			putchar((num2 % 10));
 
 			if (num1 == 98 && num2 == 99)
 				continue;
