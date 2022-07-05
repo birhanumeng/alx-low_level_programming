dfce3de99f08e439efbf9b69f518a74e77c66817
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-05 09:58:16 -0700

    print first 98 fabonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * main - program that prints the first 98 Fibonacci numbers
+  *
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+	int check;
+	unsigned long fb_num;
+	unsigned long i = 0;
+	unsigned long j = 1;
+
+	for (check = 0; check < 98; check++)
+	{
+		fb_num = i + j;
+		i = j;
+		j = fb_num;
+		printf("%lu", fb_num);
+		if (check == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
