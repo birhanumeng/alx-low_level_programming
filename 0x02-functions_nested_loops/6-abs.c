424592ae3afd16126a01441df2123a12421517a8
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-05 07:05:36 -0700

    find absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Find the absolute value of a number
+ *
+ * @n: input feild for checking
+ * Return: return the absolute value of the input number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = n * -1;
+	return (n);
+}
