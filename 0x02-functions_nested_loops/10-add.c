4b0d2b19d070e4c0a8938b0c77200d9167c453f0
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-05 08:18:36 -0700

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - adds two numbers
+ * @n1: first operand
+ * @j: second operand
+ * Return: return the result
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
