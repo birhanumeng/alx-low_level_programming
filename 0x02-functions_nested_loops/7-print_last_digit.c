555a41b105fcf94cba9cde6249e57a0e8a327442
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-05 08:57:02 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,10 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = (n * -1);
+	int last = n % 10;
+	if (last < 0)
+		n = (last * -1);
 
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	_putchar(last + '0');
+	return (last);
 }
