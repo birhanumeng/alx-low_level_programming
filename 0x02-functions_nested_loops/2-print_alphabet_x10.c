8c41aebe22c90ac8583a2f34fc3d4af86c660bf8
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-05 05:09:30 -0700

    print alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,12 +10,11 @@ void print_alphabet_x10(void)
 	char c;
 	int j;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j < 10; j++)
+		for (c = 'a'; c <= 'z'; c++)
 			_putchar(c);
 
 		_putchar('\n');
 	}
-
 }
