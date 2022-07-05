030fdcab89d64e6a230b83d68a53f9aea440d3ea
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-05 08:40:17 -0700

    print time tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,7 +21,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 			}
-			if ((check < 10) && (j !=0))
+			if ((check < 10) && (j != 0))
 			{
 				_putchar(' ');
                                 _putchar((check % 10) + '0');
