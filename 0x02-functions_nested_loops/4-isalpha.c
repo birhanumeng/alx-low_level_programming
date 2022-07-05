fc8bd041a6a4f5b97ffdd745fc227de8b1193803
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-05 06:13:35 -0700

    is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * __isalpha - check if the input is English alphabet
+ *
+ * @c: input feild for checking
+ * Return: 1 if input is alphabet otherwise 0
+ */
+int __isalpha(int c)
+{
+	if ((c > 96) && (c < 123) || (c > 64) && (c < 91))
+		return (1);
+	return (0);
+}
