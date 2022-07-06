485ceec947d8f614249f7c58463218f3b64f61f5
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-05 23:57:34 -0700

    print time tabele between 0 & 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,43 +8,41 @@
  */
 void print_times_table(int n)
 {
-	int i, j, check;
+	int i;
+	int j;
+	int check;
 
-	if (n >= 0 && n <= 15)
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			check = (i * j);
+			if (j != 0)
 			{
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				check = (i * j);
-				if (check < 10 && j != 0)
-				{
-					_putchar(' ');
-					_putchar(check + '0');
-				}
-				else if (check >= 100)
-				{
-					_putchar((check / 100) + '0');
-					_putchar(((check % 100) / 10) + '0');
-					_putchar(((check % 100) % 10) + '0');
-				}
-				else if ((check >= 10) && (check < 100))
-				{
-					_putchar((check / 10) + '0');
-					_putchar((check % 10) + '0');
-				}
-				else
-				{
-					_putchar((check % 10) + '0');
-				}
+				_putchar(',');
+				_putchar(' ');
 			}
-			_putchar('\n');
+			if (check < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar(check + '0');
+			}
+			else if (check >= 100)
+			{
+				_putchar((check / 100) + '0');
+				_putchar(((check % 100) / 10) + '0');
+				_putchar(((check % 100) % 10) + '0');
+			}
+			else if ((check >= 10) && (check < 100))
+			{
+				_putchar((check / 10) + '0');
+				_putchar((check % 10) + '0');
+			}
+			else
+			{
+				_putchar((check % 10) + '0');
+			}
 		}
+		 _putchar('\n');
 	}
-	_putchar('\n');
 }
