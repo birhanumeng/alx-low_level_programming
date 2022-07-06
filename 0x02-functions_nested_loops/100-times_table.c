79c419ef5c7b862a7723d39decae433b4484dfb3
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-05 23:26:47 -0700

    print time tabele between 0 & 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *print_times_table - Print times table between 0 ad 15
  *
@@ -7,43 +8,43 @@
  */
 void print_times_table(int n)
 {
-	int i, j, check;
+        int i, j, check;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i < 10; i++)
-		{
-			for (j = 0; j < 10; j++)
-			{
-				if (j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				check = (i * j);
-				if (check < 10 && j != 0)
-				{
-					_putchar(' ');
-					_putchar(check + '0');
-				}
-				else if (check >= 100)
-				{
-					_putchar((check / 100) + '0');
-					_putchar(((check % 100) / 10) + '0');
-					_putchar(((check % 100) % 10) + '0');
-				}
-				else if ((check >= 10) && (check < 100))
-				{
-					_putchar((check / 10) + '0');
-					_putchar((check % 10) + '0');
-				}
-				else
-				{
-					_putchar((check % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
-	_putchar('\n');
+        if (n >= 0 && n <= 15)
+        {
+                for (i = 0; i < 10; i++)
+                {
+                        for (j = 0; j < 10; j++)
+                        {
+                                if (j != 0)
+                                {
+                                        _putchar(',');
+                                        _putchar(' ');
+                                }
+                                check = (i * j);
+                                if (check < 10 && j != 0)
+                                {
+                                        _putchar(' ');
+                                        _putchar(check + '0');
+                                }
+                                else if (check >= 100)
+                                {
+                                        _putchar((check / 100) + '0');
+                                        _putchar(((check % 100) / 10) + '0');
+                                        _putchar(((check % 100) % 10) + '0');
+                                }
+                                else if ((check >= 10) && (check < 100))
+                                {
+                                        _putchar((check / 10) + '0');
+                                        _putchar((check % 10) + '0');
+                                }
+                                else
+                                {
+                                        _putchar((check % 10) + '0');
+                                }
+                        }
+                        _putchar('\n');
+                }
+        }
+        _putchar('\n');
 }
