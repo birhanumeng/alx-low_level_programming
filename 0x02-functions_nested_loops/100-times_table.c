2a460ec1cf892288c1dd00dea473b56e42ec5b90
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-06 00:27:20 -0700

    print time tabele between 0 & 15

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,39 +12,42 @@ void print_times_table(int n)
 	int j;
 	int check;
 
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <=15)
 	{
-		for (j = 0; j <= n; j++)
-		{
-			check = (i * j);
-			if (j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-			if (check < 10 && j != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(check + '0');
-			}
-			else if (check >= 100)
-			{
-				_putchar((check / 100) + '0');
-				_putchar(((check % 100) / 10) + '0');
-				_putchar(((check % 100) % 10) + '0');
-			}
-			else if ((check >= 10) && (check < 100))
-			{
-				_putchar(' ');
-				_putchar((check / 10) + '0');
-				_putchar((check % 10) + '0');
-			}
-			else
-			{
-				_putchar((check % 10) + '0');
-			}
-		}
-		 _putchar('\n');
+		for (i = 0; i <= n; i++)
+        	{
+                	for (j = 0; j <= n; j++)
+                	{
+                        	check = (i * j);
+                        	if (j != 0)
+                        	{
+                                	_putchar(',');
+                                	_putchar(' ');
+                        	}
+                        	if (check < 10 && j != 0)
+                        	{
+                                	_putchar(' ');
+                                	_putchar(' ');
+                                	_putchar(check + '0');
+                        	}
+                        	else if (check >= 100)
+                        	{
+                                	_putchar((check / 100) + '0');
+                                	_putchar(((check % 100) / 10) + '0');
+                                	_putchar(((check % 100) % 10) + '0');
+                        	}
+                        	else if ((check >= 10) && (check < 100))
+                        	{
+                                	_putchar(' ');
+                                	_putchar((check / 10) + '0');
+                                	_putchar((check % 10) + '0');
+                        	}
+                        	else
+                        	{
+                                	_putchar((check % 10) + '0');
+                        	}
+                	}
+                 	_putchar('\n');
+        	}
 	}
 }
