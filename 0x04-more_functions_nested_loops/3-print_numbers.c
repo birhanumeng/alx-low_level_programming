89b92183ec0df8e3da4f65d8c0b056a7d16a74be
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-07 06:18:46 -0700

    print digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print digit from 0 to 9.
+ * 
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
