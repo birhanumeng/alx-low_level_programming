b2ec5b598dee2c9074a4a38ac7e7a5f006701e06
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-07 12:17:39 -0700

    prints integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints integer.
+ * @n: input.
+ * Return: void.
+ */
+void print_number(int n)
+{
+	unsigned int digit, check, tmp;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		tmp = n * -1;
+	}
+	else
+	{
+		tmp = n;
+	}
+
+	digit = tmp;
+	check = 1;
+
+	while (digit > 9)
+	{
+		digit /= 10;
+		check *= 10;
+	}
+
+	for (; check >= 1; check /= 10)
+	{
+		_putchar(((tmp / check) % 10) + '0');
+	}
+}
