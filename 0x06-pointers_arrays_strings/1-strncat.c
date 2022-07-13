1021dee916bbc77dd64f56e0a7bbb108436af96d
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-13 00:15:43 -0700

    concat only n bytes to dest

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - concatinate only characters from src to dest
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: number of bytes
+ * Return: concatinated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		dest[i++];
+	}
+	while ((src[j] != '\0') && (j <= n))
+	{
+		dest[i++] = src[j++];
+	}
+	dest[i] = '\0';
+
+	return dest;
+}
+
