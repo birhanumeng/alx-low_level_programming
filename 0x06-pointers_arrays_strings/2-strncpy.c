a0f8d097d512a50472256fbb6d8e06d90e2bffe8
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-13 23:30:34 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatinate only n byte characters from src in to strating 
+ *            from first index of dest. If src less than n, dest will
+ *            filled with null character.
+ * @dest: destination pointer.
+ * @src: source pointer.
+ * @n: number of bytes.
+ * Return: concatinated string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+		dest[i++] = '\0';
+
+	return (dest);
+}
