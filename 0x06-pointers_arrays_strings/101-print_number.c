c4490e42153777cdb4a6c799fa6accff2fabc756
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-14 03:12:48 -0700

    print integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - print integer.
+ * @n: input number.
+ * Return: nothing.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+		_putchar('-');
+
+	while ((n / 10 > 0) || (n % 10 > 0))
+	{
+		n = n / 10;
+		_putchar(n % 10 + '\0');
+	}
+
+	_putchar('\n');
+}
