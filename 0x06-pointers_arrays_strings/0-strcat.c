5a7282abc94ab4c285659344ca4f3b8fcb92a315
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-13 22:25:08 -0700

    concatinate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,11 +16,10 @@ char *_strcat(char *dest, char *src)
 	{
 		;
 	}
-	while (src[j] != '\0')
+	while (src[j])
 	{
 		dest[i++] = src[j++];
 	}
-	dest[i] = '\0';
 
-	return dest;
+	return (dest);
 }
