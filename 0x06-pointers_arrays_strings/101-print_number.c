ab158204c3346439052c32b292761820508f8d2b
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-14 17:46:55 -0700

    print integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,17 +9,14 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-	if (n < 0)
+	if (num < 0)
 	{
 		_putchar('-');
 		num = -num;
 	}
 
-	while ((num / 10) > 0)
-	{
-		_putchar((num % 10) + '0');
-		num = num / 10;
-	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
 	_putchar((num % 10) + '0');
 }
