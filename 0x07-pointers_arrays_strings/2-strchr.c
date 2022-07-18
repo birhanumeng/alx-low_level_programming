7d7b4f411d0da8aa01ccc4162a66fabd81191c9c
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-18 03:40:54 -0700

    search character

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _strchr - locates a character in a string
+  * @dest: character sting which a character is searching from
+  * @c: searching character
+  *
+  * Return: pointer to the found character
+  */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+
+	return ('\0');
+}
