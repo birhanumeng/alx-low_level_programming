59c2178a4e1c52f284c77a88c193d7651ad6a3ef
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-18 03:28:00 -0700

    copy from memory to memory

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,12 +12,12 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i, j;
 
 	i = 0;
-	while (*(dest + i))
+	while (dest[i])
 		i++;
 
 	for (j = 0; j < n; j++)
 	{
-		*(dest + i) = *(src +j);
+		dest[i] = src[j];
 		i++;
 	}
 
