db0fb83d3f54f5be362ad4a3c69084f4e28bf66a
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-21 01:56:58 -0700

    check for palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+int my_palindrome(int *str, int i, int j);
+/**
+ * is_palindrome - check is the string is palindrome
+ * @s: string to be checked if it is palindrome
+ * Return: 1 if a string is palindrome, otherwise 0
+ */
+int is_palindrome(char *s)
+{
+	int i = 0, len = 0, j;
+
+	j = my_strlen(s, len);
+
+	return (my_palindrome(s, i, j));
+}
+
+/**
+ * my_palindrome - check if a string is palindrome
+ * @s: string to be checked for palindrome
+ * @i: for traversing string from star point of a string
+ * @j: for traversing string from last point of a string
+ * Return: 1 if a string is palindrome, otherwise 0
+ */
+int my_palindrome(int *str, int i, int j)
+{
+	if (*(str + i) != *(str + j))
+		return (0);
+	
+	if (i >= j)
+		return (1);
+
+	my_palindrome(str, i++, j--);
+}
+
+/**
+ * my_strlen - find length of a string
+ * @str: string to be find it's length
+ * @len: holding length of a string
+ * Return: length of a string
+ */
+int my_strlen(char *str, int *len)
+{
+	if (*str != '\0')
+		my_strlrn(str++, len++);
+
+	return (len);
+}
