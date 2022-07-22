fa07e41e5ec122f33a40c22e8cb65e9acdd49614
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-22 06:48:08 -0700

    multiplication

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name
+ * @argc: Number of arguments supplied
+ * @argv: Reffers to array of strings supplied
+ *
+ * Return: 0 for success or 1 for failure
+ */
+int main(int argc, char *argv[])
+{
+	int num1 = 0, num2 = 0;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+
+		printf("%d\n", num1 * num2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
