670600896d2bfe69a15487b1e4e40af07a985671
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-22 06:18:41 -0700

    print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the program name
+ * @argc: Number of arguments supplied
+ * @argv: Reffers to array of strings supplied
+ *
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
