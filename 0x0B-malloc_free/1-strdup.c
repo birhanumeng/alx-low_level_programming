dba97894bfe9557584627848645519f456c47b2b
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-25 04:39:55 -0700

    create copy of array

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - create newly allocated space in memory,
+ *                which contains a copy of the string given as a parameter
+ * @str: original string argument
+ * Return: pointer to newly created array, or NULL if it fail or
+ *         input argument is NULL
+ */
+char *_strdup(char *str)
+{
+	char *new_str;
+
+	new_str = malloc(sizeof(str));
+	if (new_str == NULL || str == NULL)
+		return (NULL);
+
+	while (*str)
+	{
+		new_str = str;
+		str++;
+		new_str++;
+	}
+	free(new_str);
+
+	return (new_str);
+}
