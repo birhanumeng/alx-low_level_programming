a7632a9895d44d61f79a2e3aa373174ea1151a99
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-25 04:13:32 -0700

    create an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (ptr == NULL || size == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		*(ptr + i) = c;
+		ptr[i] = c;
 	free(ptr);
 
 	return (ptr);
