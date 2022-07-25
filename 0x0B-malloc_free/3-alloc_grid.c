a9117fe8f17534586c6bf26d60f8f0836d34d5af
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-25 06:34:37 -0700

    two dimentional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width < 0 || height < 0)
 		return (NULL);
 
-	multi_arr = (**int)malloc(height * sizeof(int));
+	multi_arr = (int **)malloc(height * sizeof(int));
 
 	if (multi_arr == NULL)
 		return (NULL);
