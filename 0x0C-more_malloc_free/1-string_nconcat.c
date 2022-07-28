e49b930de42a03236c02c030740e38776a441767
birhanumeng <eyerusalemmengiste@gmail.com>
2022-07-28 07:44:01 -0700

    concatinate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (k = 0; k < i; k++)
 		str[k] = s1[k];
-	for (k = 0 k < j; k++)
+	for (k = 0; k < j; k++)
 		str[i++] = s2[k];
 
 	str[i] = '\0';
