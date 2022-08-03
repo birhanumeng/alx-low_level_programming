4ae35e1f2eeb460913b2e1683379f35f26f484f2
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-03 06:16:06 -0700

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL && size > 0 && action !=NULL)
+	if (array != NULL && size > 0 && action != NULL)
 	{
 		while(size-- > 0)
 			action(*array++);
