1378e03d10a8f8862c941b676cb603fef10a59d0
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-03 06:46:57 -0700

    search an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (index = 0; index < size; index++)
 		{
-			if (cmp(array[i]))
+			if (cmp(array[index]))
 				return (index);
 		}
 	}
