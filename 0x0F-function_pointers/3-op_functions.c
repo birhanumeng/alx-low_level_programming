48413bfe6ccf8d002dc3284787e045afc0c52b2e
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-03 07:46:04 -0700

    operation function

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum the two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the sum of two numbers
+ */
+int op_add(int a, int b)
+{
+        return (a + b);
+}
+
+/**
+ * op_sub - substruct the two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the substruction of two numbers
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - multiply the two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the multplication of two numbers
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_div - divide the two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the division of two numbers
+ */
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - calculate modulo of a number
+ * @a: first number
+ * @b: second number
+ *
+ * Return: modulo of a number
+ */
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
