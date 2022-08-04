6541b1d3ec2487eb44e335d18661bbd965a85c61
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-04 03:51:43 -0700

    sum all parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,18 +10,13 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list num_param;
-	unsigned int i;
 	int sum = 0;
+	unsigned int i;
+	va_list valist;
 
-	if (n == 0)
-		return (0);
-
-	va_start(num_param, n);
+	va_start(valist, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(num_param, int);
-
-	va_end(num_param);
-
+		sum += va_arg(valist, int);
+	va_end(valist);
 	return (sum);
 }
