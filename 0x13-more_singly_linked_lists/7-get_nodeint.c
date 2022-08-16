abdd95a6f21644903b659071f3a810cfdce24ab5
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-16 06:13:41 -0700

    find the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,7 +10,7 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *previous = head, *next, *target;
+	listint_t *previous = *head, *next, *target;
 	int i = 0;
 
 	while (i < index)
