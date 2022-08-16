ed037b7411d04a97c12407e726ab0738007925eb
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-16 07:04:23 -0700

    add node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
-	unsigned int = 0;
+	unsigned int index = 0;
 
 	if (head == NULL)
 		return (NULL);
