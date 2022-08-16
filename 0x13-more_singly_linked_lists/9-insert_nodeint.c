17cc829e935165657b05651ae76d00123eeb502a
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-16 07:24:03 -0700

    add node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,21 +28,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 
+	new_node->n = n;
 	if (index == 1)
 	{
-		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
 	}
-	else if ((*head)->next == NULL)
-	{
-		new_node->n = n;
-		new_node->next = NULL;
-		(*head)->next = new_node;
-	}
 	else
 	{
-		new_node->n = n;
 		new_node->next = (*head)->next;
 		(*head)->next = new_node;
 	}
