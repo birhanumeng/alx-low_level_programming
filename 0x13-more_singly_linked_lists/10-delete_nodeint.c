181caebed93a172882d024a5f08bbab8ed8ee7de
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-16 08:06:57 -0700

    delete node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -23,7 +23,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 	current = *head;
-	while (i < index - 1)
+	for (i = 0; i < index - 1; i++)
 	{
 		if (current->next == NULL)
 			return (-1);
