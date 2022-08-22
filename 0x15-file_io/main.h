8f5633eb0a06c1673b1688408eb2310b58dbfa13
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-22 01:30:41 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,11 @@
 #define FILE_IO_H
 
 #include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
