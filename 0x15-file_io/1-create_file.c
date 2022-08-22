5f6caeffe01aa0f312119c031d5a9b5e9203c29b
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-22 04:13:50 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file
+ * @filename: refers to filename
+ * @text_content: refers to file content to writen
+ *
+ * Return: 1 on success or -1 on failer
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
