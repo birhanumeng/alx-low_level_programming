7f2b08056073ce6994776063345fc05200d99f35
birhanumeng <eyerusalemmengiste@gmail.com>
2022-08-22 03:44:51 -0700

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,30 +10,30 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t r, w;
-	char *buf;
+	ssize_t lenr, lenw;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-	r = read(fd, buf, letters);
+	lenr = read(fd, buffer, letters);
 	close(fd);
-	if (r == -1)
+	if (lenr == -1)
 	{
-		free(buf);
+		free(buffer);
 		return (0);
 	}
-	w = write(STDOUT_FILENO, buffer, r);
+	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-	if (r != w)
+	if (lenr != lenw)
 		return (0);
-	return (w);
+	return (lenw);
 }
