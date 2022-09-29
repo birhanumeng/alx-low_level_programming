4ffbb3f5eb713704d55b61963569ce6f87dc0b43
birhanumeng <eyerusalemmengiste@gmail.com>
2022-09-29 08:19:22 -0700

    sum elements in the lists

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum the elements in the list
+ * @head: head node
+ * Retrun: sum of elements
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
