288a88c80b237ca582e0f60f7805675e7bb028b9
birhanumeng <eyerusalemmengiste@gmail.com>
2022-12-07 09:45:34 -0800

    find perimeter of the island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,9 +9,12 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if(grid[i][j] == 1):
-                size += 1
-                if(i > 0 and grid[i - 1][j] == 1):
-                    neighbor += 1
-                if(j > 0 and grid[i][j - 1] == 1):
-                    neighbor += 1
-    return size * 4 - neighbor * 2
+                if grid[i - 1][j] == 0:
+                    perimeter += 1
+                if grid[i + 1][j] == 0:
+                    perimeter += 1
+                if grid[i][j - 1] == 0:
+                    perimeter += 1
+                if grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
