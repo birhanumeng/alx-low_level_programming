503e221b23de81ce9f07c5955529ffab7faaa95b
birhanumeng <eyerusalemmengiste@gmail.com>
2022-12-07 10:07:24 -0800

    find perimeter of the island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,16 +3,7 @@
 
 
 def island_perimeter(grid):
-    """returns perimeter of island.
-
-    The grid represents an island which is
-    0 for water and 1 for land
-
-    Args:
-        grid: list of lists 0f 0 and 1 representing
-              the island
-    Returns: perimeter of the island
-    """
+    """returns perimeter of island."""
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
